d7b930d1916a3cf2a2d211aaa578a00038f2933c
mateus100 <mateusrodriguesmoreira20@gmail.com>
2017-06-04 14:38:17 -0300

    Python 6° dia Aprensentação feita

--- pydaria.py
+++ pydaria.py
@@ -1,21 +1,21 @@
-#Quanto lucra uma padaria por pão feito
+#Joguinho de ser um padeiro
+
+
+
+def apresentacao():
+    nome = input("Olá qual é o seu nome? ")    
+    print("Como vai %s?" %nome)
+    print("Você %s agora trabalha para uma padaria " %nome)
+    print("Pronto para aprender fazer pães? Aperte a tecla ENTER: ")
+
+apresentacao()
 
 def receita():
-   print("Pronto para aprender fazer pães? Aperte qualquer tecla: ")
    input("")
-   print("A receita para fazer 1 pão é:\n 1 Ovo \n 5 colheres de Açúcar \n 1 colher de sopa de sal \n 1 kg de farinha de trigo \n 2 tabletes de fermento...")
+   print("A receita para fazer 1 pão é:\n 1 Ovo \n 5 colheres de Açúcar \n 1 colher de sopa de sal \n 1 kg de farinha de trigo \n 2 tabletes de fermento... Quantos pães você fará hoje?")
 
 receita()
-print("Quantos pães fará hoje? ")
-ovo = int(input("Quantos ovos? "))
 
-sugar = int(input("Quantas colheres de açúcar? "))
-
-trigo = int(input("Quantos kg de farinha de trigo? "))
-
-sal = int(input("Quantas colheres de Sal? "))
-
-fermento = int(input("Quantos tabletes de fermento? "))
 
 def fazerpao(ovo, sugar, trigo, sal, fermento):
     if ovo >= 1 and trigo >= 1:
@@ -31,8 +31,17 @@ def fazerpao(ovo, sugar, trigo, sal, fermento):
     if pao < 9:
         print("O pão saiu ruim, deve está faltando alguma coisa...")
     elif pao >= 10:
-        result = pao % 9
+        result = pao / 9
         print("O total de pães são %d " %(result) )
        
+ovo = int(input("Quantos ovos? "))
+                                        
+sugar = int(input("Quantas colheres de açúcar? "))
+
+trigo = int(input("Quantos kg de farinha de trigo? "))
+
+sal = int(input("Quantas colheres de Sal? "))
+
+fermento = int(input("Quantos tabletes de fermento? "))
 
 fazerpao(ovo,sugar,trigo,sal,fermento)
