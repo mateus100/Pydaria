ec9b9152fdd260f477d90cba3512cdae5c69ec06
Mateus <Mateus R. Moreira>
2017-12-04 22:42:15 -0300

    Receita string add

--- pydaria.py
+++ pydaria.py
@@ -1,22 +1,22 @@
 #Joguinho de ser um padeiro
-
-
-
 def apresentacao():
     nome = input("Olá qual é o seu nome? ")    
     print("Como vai %s?" %nome)
     print("Você %s agora trabalha para uma padaria " %nome)
     print("Pronto para aprender fazer pães? Aperte a tecla ENTER: ")
 
-apresentacao()
 
 def receita():
-   input("")
-   print("A receita para fazer 1 pão é:\n 1 Ovo \n 5 colheres de Açúcar \n 1 colher de sopa de sal \n 1 kg de farinha de trigo \n 2 tabletes de fermento... Quantos pães você fará hoje?")
-
-receita()
 
 
+   receita = '''A receita para fazer 1 pão é:
+   1 Ovo
+   5 colheres de Açúcar
+   1 colher de sopa de sal
+   1 kg de farinha de trigo
+   2 tabletes de fermento... Quantos pães você fará hoje?
+   '''
+   print(receita)
 def fazerpao(ovo, sugar, trigo, sal, fermento):
     if ovo >= 1 and trigo >= 1:
         if sugar >= 5 and sal >= 1 and fermento >= 2:
@@ -34,14 +34,19 @@ def fazerpao(ovo, sugar, trigo, sal, fermento):
         result = pao / 9
         print("O total de pães são %d " %(result) )
        
-ovo = int(input("Quantos ovos? "))
-                                        
-sugar = int(input("Quantas colheres de açúcar? "))
+apresentacao()
+input("")
+receita()
+def make():
+  ovo = int(input("Quantos ovos? "))
+                                          
+  sugar = int(input("Quantas colheres de açúcar? "))
 
-trigo = int(input("Quantos kg de farinha de trigo? "))
+  trigo = int(input("Quantos kg de farinha de trigo? "))
 
-sal = int(input("Quantas colheres de Sal? "))
+  sal = int(input("Quantas colheres de Sal? "))
 
-fermento = int(input("Quantos tabletes de fermento? "))
+  fermento = int(input("Quantos tabletes de fermento? "))
 
-fazerpao(ovo,sugar,trigo,sal,fermento)
+  fazerpao(ovo,sugar,trigo,sal,fermento)
+make()
