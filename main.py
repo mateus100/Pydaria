1f07228216830cbfd1027acb69946e840cd0877e
Mateus R. Moreira <mrm220396@yahoo.com>
2018-02-10 22:54:48 -0300

    Version 0.0.1

--- main.py
+++ main.py
@@ -1,52 +1,50 @@
 #Joguinho de ser um padeiro
+import configparser
+init = configparser.ConfigParser()
+
+init.read_file(open('receita.ini'))
+
 def apresentacao():
     nome = input("Olá qual é o seu nome? ")    
-    print("Como vai %s?" %nome)
-    print("Você %s agora trabalha para uma padaria " %nome)
-    print("Pronto para aprender fazer pães? Aperte a tecla ENTER: ")
+    print(f'Como vai {nome}?')
+    print(f'Você {nome} agora trabalha para uma padaria\n{nome}Pronto para aprender fazer pães?\nAperte a tecla ENTER: ')
+    input("")
 
 
 def receita():
+   receita = init['RECEITA']['bolo']
+   return receita
 
-
-   receita = '''A receita para fazer 1 pão é:
-   1 Ovo
-   5 colheres de Açúcar
-   1 colher de sopa de sal
-   1 kg de farinha de trigo
-   2 tabletes de fermento... Quantos pães você fará hoje?
-   '''
-   print(receita)
 def fazerpao(ovo, sugar, trigo, sal, fermento):
     if ovo >= 1 and trigo >= 1:
         if sugar >= 5 and sal >= 1 and fermento >= 2:
-           print("Ingredientes prontos")
+           return "Ingredientes prontos"
         else:
-           print("Veja se o açúcar e sal são o suficientes \n ou o fermento pode está em falta")
+           return "Veja se o açúcar e sal são o suficientes \n ou o fermento pode está em falta"
     else:
-        print("Ingredientes insufissientes")
+        return "Ingredientes insufissientes"
     
     pao = ovo + sugar + trigo + sal + fermento
 
     if pao < 9:
-        print("O pão saiu ruim, deve está faltando alguma coisa...")
+        return "O pão saiu ruim, deve está faltando alguma coisa..."
     elif pao >= 10:
         result = pao / 9
-        print("O total de pães são %d " %(result) )
+        return "O total de pães são %d " %(result) 
        
 apresentacao()
-input("")
+
 receita()
 def make():
-  ovo = int(input(init['PERGUNTAS']['ovo']))
+  ovo = int(input(init['PERGUNTAS']['ovo'] ))
                                           
-  sugar = int(input(init['PERGUNTAS']['acucar']))
+  sugar = int(input(init['PERGUNTAS']['acucar'] ))
 
-  trigo = int(input(init['PERGUNTAS']['trigo']))
+  trigo = int(input(init['PERGUNTAS']['trigo'] ))
 
-  sal = int(input(init['PERGUNTAS']['sal']))
+  sal = int(input(init['PERGUNTAS']['sal'] ))
 
-  fermento = int(input(init['PERGUNTAS']['fermento']))
+  fermento = int(input(init['PERGUNTAS']['fermento'] ))
 
-  fazerpao(ovo,sugar,trigo,sal,fermento)
+  print(fazerpao(ovo,sugar,trigo,sal,fermento))
 make()
